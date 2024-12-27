ed47ed2cf90cef1a13da591e278b5e69388827db
Rudenko Volodymyr <rudenkovladimir2003@gmail.com>
2024-12-27 16:39:09 +0200

    add api paths constants

---
+++ src/main/java/com/vrudenko/kanban_board/constant/ApiPaths.java
@@ -0,0 +1,12 @@
+package com.vrudenko.kanban_board.constant;
+
+public final class ApiPaths {
+  public static final String BOARDS = "/boards";
+  public static final String BOARD_ID = "/{boardId}";
+
+  public static final String COLUMNS = "/columns";
+
+  public static final String TASKS = "/tasks";
+
+  public static final String SUB_TASKS = "/subtasks";
+}
