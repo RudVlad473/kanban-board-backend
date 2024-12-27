ab41b9ff9c6cbb19c1b53435761803a9d9844097
Rudenko Volodymyr <rudenkovladimir2003@gmail.com>
2024-12-27 16:21:16 +0200

    add board controller

--- src/main/java/com/vrudenko/kanban_board/controller/BoardController.java
+++ src/main/java/com/vrudenko/kanban_board/controller/BoardController.java
@@ -1,19 +1,28 @@
 package com.vrudenko.kanban_board.controller;
 
-import com.vrudenko.kanban_board.entity.BoardEntity;
+import com.vrudenko.kanban_board.dto.BoardResponseDTO;
+import com.vrudenko.kanban_board.dto.SaveBoardRequestDTO;
 import com.vrudenko.kanban_board.service.BoardService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
+@RequestMapping("/boards")
 public class BoardController {
   @Autowired private BoardService boardService;
 
-  @RequestMapping("/boards")
-  public List<BoardEntity> getAllBoards() {
-    return this.boardService.getAllBoards();
+  @GetMapping
+  public List<BoardResponseDTO> findAll() {
+    return this.boardService.findAll();
+  }
+
+  @PostMapping
+  public BoardResponseDTO save(@RequestBody SaveBoardRequestDTO dto) {
+    return this.boardService.save(dto);
   }
 }
