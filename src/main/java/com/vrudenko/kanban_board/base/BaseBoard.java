207873b9f9601f592269586724998a8aa4e31a47
Rudenko Volodymyr <rudenkovladimir2003@gmail.com>
2024-12-27 16:21:06 +0200

    add base classes

---
+++ src/main/java/com/vrudenko/kanban_board/base/BaseBoard.java
@@ -0,0 +1,10 @@
+package com.vrudenko.kanban_board.base;
+
+/**
+ * Interfaces are used for base entities instead of classes because you can't override fields in
+ * Java therefore you can't override field to apply Hibernate's annotations to them so methods are
+ * used instead
+ */
+public interface BaseBoard {
+  String getName();
+}
---
+++ src/main/java/com/vrudenko/kanban_board/base/BaseId.java
@@ -0,0 +1,5 @@
+package com.vrudenko.kanban_board.base;
+
+public interface BaseId {
+  String getId();
+}
