54710558ea3a30c216537de472c1543d983ac59e
Rudenko Volodymyr <rudenkovladimir2003@gmail.com>
2025-04-22 16:19:42 +0300

    add bean configuration

---
+++ src/main/java/com/vrudenko/kanban_board/config/BeanConfiguration.java
@@ -0,0 +1,36 @@
+package com.vrudenko.kanban_board.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+@Configuration
+@RequiredArgsConstructor
+public class BeanConfiguration {
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    return http.getSharedObject(AuthenticationManagerBuilder.class).build();
+  }
+
+  @Bean
+  public HttpSessionEventPublisher httpSessionEventPublisher() {
+    return new HttpSessionEventPublisher();
+  }
+
+  @Bean
+  public DataFactory dataFactory() {
+    return new DataFactory();
+  }
+}
