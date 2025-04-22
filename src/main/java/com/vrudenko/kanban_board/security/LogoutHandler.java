efbcc931e75a1d3b45e854ab6123e96aa27f9219
Rudenko Volodymyr <rudenkovladimir2003@gmail.com>
2025-04-22 16:20:43 +0300

    add security (wip)

---
+++ src/main/java/com/vrudenko/kanban_board/security/LogoutHandler.java
@@ -0,0 +1,25 @@
+package com.vrudenko.kanban_board.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class LogoutHandler implements LogoutSuccessHandler {
+
+  @Override
+  public void onLogoutSuccess(
+      HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+      throws IOException, ServletException {
+
+    // Set response status and body (e.g., JSON)
+    response.setStatus(HttpServletResponse.SC_OK);
+    response.setContentType("application/json");
+    response.getWriter().write("{\"message\": \"Successfully logged out\"}");
+  }
+}
---
+++ src/main/java/com/vrudenko/kanban_board/security/SecurityConfiguration.java
@@ -0,0 +1,65 @@
+package com.vrudenko.kanban_board.security;
+
+import com.vrudenko.kanban_board.constant.ApiPaths;
+import com.vrudenko.kanban_board.constant.SecurityConstants;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+  private final AuthenticationProvider authenticationProvider;
+  private final LogoutHandler handlerLogout;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    var securityContextRepository = new HttpSessionSecurityContextRepository();
+
+    // csrf & cors
+    http.csrf(AbstractHttpConfigurer::disable).cors(Customizer.withDefaults());
+
+    // storing the session
+    http.securityContext((context) -> context.securityContextRepository(securityContextRepository));
+
+    // session management
+    http.sessionManagement(
+        (session) -> {
+          session.maximumSessions(1).maxSessionsPreventsLogin(true);
+          session.sessionFixation(
+              SessionManagementConfigurer.SessionFixationConfigurer::newSession);
+          session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+        });
+
+    // clear cookie when logout
+    http.logout(
+        (logout) -> {
+          logout.logoutUrl(ApiPaths.LOGOUT);
+          logout.addLogoutHandler(
+              new HeaderWriterLogoutHandler(
+                  new ClearSiteDataHeaderWriter(ClearSiteDataHeaderWriter.Directive.COOKIES)));
+          logout.deleteCookies(SecurityConstants.SESSION_NAME);
+          logout.logoutSuccessHandler(handlerLogout);
+        });
+
+    // auth provider for connect DAO
+    http.authenticationProvider(authenticationProvider);
+
+    return http.build();
+  }
+}
