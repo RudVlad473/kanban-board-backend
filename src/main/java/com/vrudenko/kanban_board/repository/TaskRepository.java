c8a89051466b0fd4199d60f5eff1d489e3a4a3ec
Rudenko Volodymyr <rudenkovladimir2003@gmail.com>
2025-05-06 14:16:31 +0300

    feat: add TaskMapper interface for task DTO conversion

---
+++ src/main/java/com/vrudenko/kanban_board/repository/TaskRepository.java
@@ -0,0 +1,12 @@
+package com.vrudenko.kanban_board.repository;
+
+import com.vrudenko.kanban_board.entity.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<TaskEntity, String> {
+  List<TaskEntity> findAllByColumnId(String columnId);
+
+  void deleteAllByColumnId(String columnId);
+}
