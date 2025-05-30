01c4173afe81ae52a23b004081c592662f93b6e2
Rudenko Volodymyr <rudenkovladimir2003@gmail.com>
2025-05-30 16:30:12 +0300

    feat: add TASK_ID path variable to ApiPaths for task identification

--- src/main/java/com/vrudenko/kanban_board/constant/ApiPaths.java
+++ src/main/java/com/vrudenko/kanban_board/constant/ApiPaths.java
@@ -11,6 +11,7 @@ public final class ApiPaths {
   public static final String COLUMN_ID = "/{columnId}";
 
   public static final String TASKS = "/tasks";
+  public static final String TASK_ID = "/{taskId}";
 
   public static final String SUB_TASKS = "/subtasks";
 
