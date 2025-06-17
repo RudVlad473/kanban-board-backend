62c6984ae656fa1c4a56c4bad94eb3033b0a747d
Rudenko Volodymyr <rudenkovladimir2003@gmail.com>
2025-06-17 17:01:30 +0300

    feat: add method to retrieve all subtasks by task ID in SubtaskRepository

--- src/main/java/com/vrudenko/kanban_board/repository/SubtaskRepository.java
+++ src/main/java/com/vrudenko/kanban_board/repository/SubtaskRepository.java
@@ -3,6 +3,10 @@ package com.vrudenko.kanban_board.repository;
 import com.vrudenko.kanban_board.entity.SubtaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubtaskRepository extends JpaRepository<SubtaskEntity, String> {
     void deleteAllByTaskId(String taskId);
+
+    List<SubtaskEntity> findAllByTaskId(String taskId);
 }
